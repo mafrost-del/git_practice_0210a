076c2a2b66a67d0fc994dff33a937f30212e9039
Matt Frost <mafrost@deloitte.com>
2023-02-10 19:02:50 -0500

    py file updates

--- numbers.py
+++ numbers.py
@@ -1,3 +1,7 @@
-for i in range(5):
-    print("Hi how are you doing?")
-    print(i)
+for i in range(7):
+    if i < 3:
+        print(i, " Hi how are you doing?")
+    elif i > 3:
+        print(i, " Hi how are you doing?")
+    else:
+        print(i, " OMG it's number 3")
